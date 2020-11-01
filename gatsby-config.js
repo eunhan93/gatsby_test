e4da5233e064cb594c3220bd39ee60223747accf
eunhan53 <eunhan0503@gmail.com>
2020-11-01 10:05:58 +0900

    2020-11-01

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Kea Starter',
+    description: 'Gatsby starter with redux and sagas made simpler by https://kea.js.org',
+    author: 'Benjamin Glitsos',
+  },
+};
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import Counter from '../components/counter';
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Simple Counter</h1>
+    <Counter />
+  </Layout>
+);
+
+export default IndexPage;
